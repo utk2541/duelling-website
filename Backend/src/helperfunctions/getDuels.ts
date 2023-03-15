713df8a85e473e024c9e3820c911828e44e8ba5d
utk2541 <utk2541l@gmail.com>
2023-03-16 03:12:20 +0530

    BUG FIX 6

--- Backend/src/helperfunctions/getDuels.ts
+++ Backend/src/helperfunctions/getDuels.ts
@@ -4,8 +4,10 @@ export const getDuels = async (cfId: string) => {
   const duelrepo = Data.getRepository(duels);
   const [duellist, _] = await duelrepo.findAndCount({
     where: [
-      { duelistA: cfId, status: "ACCEPTED" || "PENDING" },
-      { duelistB: cfId, status: "ACCEPTED" || "PENDING" },
+      { duelistA: cfId, status: "ACCEPTED" },
+      { duelistB: cfId, status: "ACCEPTED" },
+      { duelistA: cfId, status: "PENDING" },
+      { duelistB: cfId, status: "PENDING" },
     ],
   });
 
