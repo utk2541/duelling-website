cc4e88f70cbabe510765884e611c76451c686f6f
utk2541 <utk2541l@gmail.com>
2023-03-15 23:45:01 +0530

    pls hoja

--- Backend/dist/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.constants = void 0;
-exports.constants = {
-    url: "https://codeforces.com/api",
-    frontend: "http://localhost:3000"
-};
-//# sourceMappingURL=constants.js.map
