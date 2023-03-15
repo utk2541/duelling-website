7fad070c20ab65ede033adf2973e63b081902f94
utk2541 <utk2541l@gmail.com>
2023-03-16 02:40:36 +0530

    bug fix 4

--- Backend/src/helperfunctions/createDuel.ts
+++ Backend/src/helperfunctions/createDuel.ts
@@ -1,21 +1,19 @@
 import { Data } from "../Data";
 import { duelists } from "../entities/duelists";
-import { allProblems,dproblem } from "../entities/allProblems";
+import { allProblems, dproblem } from "../entities/allProblems";
 import { Between } from "typeorm";
 import { cfapi } from "./api_helper";
 import { duels } from "../entities/duels";
 import { constants } from "../constants";
 
 interface duel {
-  rmin : number;
-  rmax : number;
-  numofproblem : number;
-  duelistA : string;
-  duelistB : string;
+  rmin: number;
+  rmax: number;
+  numofproblem: number;
+  duelistA: string;
+  duelistB: string;
 }
-const findproblems = async (
-  query : duel
-) => {
+const findproblems = async (query: duel) => {
   const problemsrepo = Data.getRepository(allProblems);
   const url = constants.url;
   const [problems, _] = await problemsrepo.findAndCount({
@@ -28,13 +26,15 @@ const findproblems = async (
 
   const count: Map<number, Array<dproblem>> = new Map();
 
-  let finalproblems : { status : string ; problems : Array<dproblem>} = { status: "OK", problems: Array<dproblem>() };
+  let finalproblems: { status: string; problems: Array<dproblem> } = {
+    status: "OK",
+    problems: Array<dproblem>(),
+  };
 
   const fetchA = await cfapi(`${url}/user.status?handle=${query.duelistA}`);
   const fetchB = await cfapi(`${url}/user.status?handle=${query.duelistB}`);
 
-  let doneprobsA : Array<any>,
-    doneprobsB : Array<any>;
+  let doneprobsA: Array<any>, doneprobsB: Array<any>;
   if (fetchA.status === "OK" && fetchB.status === "OK") {
     console.log(fetchA);
     doneprobsA = fetchA.result;
@@ -46,11 +46,15 @@ const findproblems = async (
       B = undefined;
     if (doneprobsA.length !== 0)
       A = doneprobsA.find((e) => {
-        e.problem.index == prob.id && e.problem.contestId == prob.contestId;
+        return (
+          e.problem.index == prob.id && e.problem.contestId == prob.contestId
+        );
       });
     if (doneprobsB.length !== 0)
       B = doneprobsB.find((e) => {
-        e.problem.index == prob.id && e.problem.contestId == prob.contestId;
+        return (
+          e.problem.index == prob.id && e.problem.contestId == prob.contestId
+        );
       });
 
     if (A == undefined && B == undefined) {
@@ -59,16 +63,16 @@ const findproblems = async (
           contestId: prob.contestId,
           index: prob.index,
           name: prob.name,
-          solvedBy: "none"
+          solvedBy: "none",
         };
         count.set(prob.rating, [newProb]);
       } else if ((count.get(prob.rating)?.length || 0) < query.numofproblem) {
-        let temp: Array<dproblem> = (count.get(prob.rating) || [] );
+        let temp: Array<dproblem> = count.get(prob.rating) || [];
         let newProb: dproblem = {
           contestId: prob.contestId,
           index: prob.index,
           name: prob.name,
-          solvedBy: "none"
+          solvedBy: "none",
         };
         temp.push(newProb);
         count.set(prob.rating, temp);
@@ -77,7 +81,11 @@ const findproblems = async (
   });
   count.forEach((val, key) => {
     console.log(val, key);
-    if (val.length < query.numofproblem && key >= query.rmin && key <= query.rmax) {
+    if (
+      val.length < query.numofproblem &&
+      key >= query.rmin &&
+      key <= query.rmax
+    ) {
       finalproblems.status = "NOT ENOUGH PROBLEMS IN GIVEN RANGE";
       console.log(val.length, query.numofproblem, key);
     } else
@@ -85,12 +93,13 @@ const findproblems = async (
         finalproblems.problems.push(e);
       });
   });
-  if (finalproblems.problems.length == 0) finalproblems.status = "NOT ENOUGH PROBLEMS IN GIVEN RANGE";
+  if (finalproblems.problems.length == 0)
+    finalproblems.status = "NOT ENOUGH PROBLEMS IN GIVEN RANGE";
   console.log(finalproblems);
   return finalproblems;
 };
 
-export const createDuel = async (data : duel) => {
+export const createDuel = async (data: duel) => {
   const duelistB = data.duelistB;
   const duelistrepo = Data.getRepository(duelists);
   const [_, count] = await duelistrepo.findAndCount({
